1d369e0dc15a0697d5bba385d0cc574f67d006c2
Arthur Madec-Prévost <arthur.madecprevost@protonmail.com>
2024-05-13 23:15:25 +0200

    📝 Create folders for year and month if they don't exist

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import datetime
+import os
 import requests
 import csv
 
 # Define the start and end dates
-start_date = "2024-04-12"
+start_date = "2024-01-01"
 end_date = "2024-05-13"
 
 # Convert the dates to datetime objects
@@ -15,6 +16,7 @@ current_datetime = start_datetime
 while current_datetime <= end_datetime:
     # Format the current date as a string
     current_date = current_datetime.strftime("%Y-%m-%d")
+    print(current_date)
 
     # Define the URL for the current day
     url = f"https://data.airpl.org/api/v1/mesure/horaire/?&date_heure_tu__range={current_date},{current_date}%2023:00:00&code_configuration_de_mesure__code_point_de_prelevement__code_station__code_commune__code_departement__in=44,49,53,72,85,&export=csv"
@@ -25,9 +27,18 @@ while current_datetime <= end_datetime:
     # Check if the request was successful
     if response.status_code == 200:
         data = response.content
-        # Faites quelque chose avec les données récupérées
+        
+        # Create the year and month folders if they don't exist
+        year_folder = current_datetime.strftime("%Y")
+        month_folder = current_datetime.strftime("%m")
+        year_path = f"data/{year_folder}"
+        month_path = f"data/{year_folder}/{month_folder}"
+        os.makedirs(year_path, exist_ok=True)
+        os.makedirs(month_path, exist_ok=True)
+
         # Define the path to the CSV file
-        csv_file = f"data/file_{current_date}.csv"
+        csv_file = f"data/{year_folder}/{month_folder}/file_{current_date}.csv"
+
         # Decode the response content as a string
         data_str = data.decode("utf-8")
         # Split the data into lines
